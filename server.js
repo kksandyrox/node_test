0017b2ffca391f03a97cd1ff2cf1a19beb7ec290
Sandeep Kumar <kksandyrox@gmail.com>
2014-05-05 14:02:04 +0530

    sda

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var httpd = require('http').createServer(handler);
 var fs = require('fs');
-httpd.listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP, function() {
+httpd.listen(process.env.OPENSHIFT_NODEJS_PORT||4000, process.env.OPENSHIFT_NODEJS_IP, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
 function handler(req, res) {//handler is a requestListener Function that is executed when request event is emitted
